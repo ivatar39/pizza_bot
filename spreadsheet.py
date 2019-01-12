f4d0db86e1e0b767a0b170eaf01b67a3be6b85bf
ivan <ivlupsecond@gmail.com>
2019-01-13 00:29:02 +0200

    small enhancements

--- spreadsheet.py
+++ spreadsheet.py
@@ -6,12 +6,12 @@ scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/au
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_server.json', scope)
 client = gspread.authorize(creds)
 
-sheet = client.open('pizza_bot').sheet1
+sheet_pizza = client.open('pizza_bot').get_worksheet(0)
+sheet_sushi = client.open('pizza_bot').get_worksheet(1)
+pp = pprint
 
 #all shenanigans below
 
-column = ["pepperoni", "margarita", "mushrooms", "pizza", "huiza"]
-for i in range(1, len(column)):
-    sheet.update_cell(i, 1, column[i])
-
+pp.pprint(sheet_pizza.get_all_values())
+pp.pprint(sheet_sushi.get_all_values())
 
